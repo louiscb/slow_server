963d89050e3100fda0706d49582bdeb4516bd311
Louis Cameron Booth <me@louiscb.com>
2023-05-31 11:45:10 +0200

    Add fibonacci calculation

--- lib/slow_server_web/controllers/api_controller.ex
+++ lib/slow_server_web/controllers/api_controller.ex
@@ -2,8 +2,8 @@ defmodule SlowServerWeb.ApiController do
   use SlowServerWeb, :controller
 
   def ping(conn, _params) do
-    # wait 5 seconds before responding
-    Process.sleep(5000)
+    fib = SlowServer.Fibonacci.fib(100_000)
+    IO.inspect(fib)
     resp(conn, 200, "pong")
   end
 end
