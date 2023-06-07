35bf9e280103498433302704c269376cd5684b23
Louis Cameron Booth <me@louiscb.com>
2023-06-07 22:02:30 +0200

    Clean up api

--- lib/slow_server_web/controllers/api_controller.ex
+++ lib/slow_server_web/controllers/api_controller.ex
@@ -1,10 +1,13 @@
 defmodule SlowServerWeb.ApiController do
   use SlowServerWeb, :controller
 
-  def ping(conn, _params) do
-    # fib = SlowServer.Fibonacci.fib(100_000)
-    # IO.inspect(fib)
-    d = Req.get!("https://api.github.com/repos/elixir-lang/elixir").body["description"]
-    resp(conn, 200, d)
+  def desc(conn, _params) do
+    desc = Req.get!("https://api.github.com/repos/elixir-lang/elixir").body["description"]
+    resp(conn, 200, desc)
+  end
+
+  def fib(conn, %{"n" => n}) do
+    fib = SlowServer.Fibonacci.fib(n)
+    resp(conn, 200, Integer.to_string(fib))
   end
 end
--- lib/slow_server_web/router.ex
+++ lib/slow_server_web/router.ex
@@ -8,23 +8,7 @@ defmodule SlowServerWeb.Router do
   scope "/api", SlowServerWeb do
     pipe_through :api
 
-    get "/ping", ApiController, :ping
-  end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:slow_server, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      live_dashboard "/dashboard", metrics: SlowServerWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
+    get "/desc", ApiController, :desc
+    post "/fib", ApiController, :fib
   end
 end
